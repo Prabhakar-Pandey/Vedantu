91a92dec23d7c550647d2011308fa1bf052824e4
prabhakar.pandey <vky535kolkata@gmail.com>
2021-03-20 00:32:37 +0530

    chai assertion library added and logic for registration added

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-class SortingHat{
-    constructor({capacity}) {
+/***
+ * Assumptions
+ * ScoolClass input is string [A,B]
+ * foodPrefererence input string [V,NV]
+ */
+
+class SortingHat {
+    constructor({ capacity }) {
         this.houseCapacity = capacity;
+        this.boardingHouses = {
+            "NA": []
+        };
+        this.rollMap = {}
     }
-    
-      
+    register(roll, schoolClass, foodPreferece) {
+        schoolClass = String(schoolClass).toUpperCase();
+        foodPreferece = String(foodPreferece).toUpperCase();
+        if (isNaN(roll)) {
+            return "Enter roll number integer value";
+        }
+        if (String(roll).length < 4) {
+            return "Enter roll number in 4 digits";
+        }
+
+        if (this.rollMap[roll]) {
+            return this.boardingHouses;
+        }
+        this.houseCapacity -= 1;
+        this.rollMap[roll] = `${schoolClass}-${foodPreferece}`;
+        if (this.houseCapacity < 0) {
+            return this.boardingHouses.NA.push(roll)
+        }
+        if (this.boardingHouses[`${schoolClass}${foodPreferece}`]) {
+            this.boardingHouses[`${schoolClass}${foodPreferece}`].push(roll)
+        } else {
+            this.boardingHouses[`${schoolClass}${foodPreferece}`] = [roll]
+        }
+        return this.boardingHouses;
+    }
+    getBoardingHouses() {
+        return this.boardingHouses;
+    }
+
 }
+const School = new SortingHat({
+    capacity: 10
+})
 
 module.exports = SortingHat;
+
+
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 const SortingHat = require("../index");
 beforeEach(()=> {
     
@@ -10,5 +10,33 @@ describe('Class Initialized', ()=> {
     })
   it('This will initialize my SortingHat class with Hat capacity 12 ', ()=> {
     assert.equal(School.houseCapacity, 12);
+  });
+  it('This will execute registration with input(1234,B,V) ', ()=> {
+    const boardingHouses = School.register(1000,"B","V");
+    assert.deepEqual(boardingHouses, {"BV":[1000],"NA":[]});
+  });
+  it('This will execute registration with input(1,B,V) and will be validated with digit length ', ()=> {
+    const boardingHouses = School.register(1,"B","V");
+    assert.equal(boardingHouses, "Enter roll number in 4 digits");
+  });
+  it('This will execute registration with input(a123,B,V) and will be validated with string', ()=> {
+    const boardingHouses = School.register("a123","B","V");
+    assert.equal(boardingHouses, "Enter roll number integer value");
+  });
+  it('This will add all the test cases', ()=> {
+    School.register(1000,"B","V");
+    School.register(1001,"a","V");
+    School.register(1002,"a","V");
+    School.register(1003,"b","nV");
+    School.register(1004,"b","V");
+    School.register(1005,"a","nV");
+    School.register(1007,"a","V");
+    School.register(1008,"a","nV");
+    School.register(1009,"b","nV");
+    School.register(1010,"b","V");
+    School.register(1011,"a","nV");
+    School.register(1012,"B","nV");
+    const boardingHouses = School.getBoardingHouses()
+    assert.hasAllKeys(boardingHouses, ['BV', 'AV', 'BNV','ANV','NA']);
   });
 });
